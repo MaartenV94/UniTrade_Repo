ad772c840a64e006c3fe269149d43c6d3af00071
maartenvanderbeeken <maartenvanderbeeken@hotmail.com>
2024-02-05 11:33:33 +0000

    Added an order table in the database.
    When users click the payment button, their cart items are moved into an order table and then remove the same items from the cart.

--- UniTrade_Project/resources/views/home/showcart.blade.php
+++ UniTrade_Project/resources/views/home/showcart.blade.php
@@ -66,7 +66,17 @@
     @include('home.header')
     <!-- end header section -->
 
+    @if(session()->has('message'))
 
+        <div class="alert alert-success">
+
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+            {{session()->get('message')}}
+
+        </div>
+
+    @endif
+    
 <div class="center">
 
     <table>
@@ -100,6 +110,15 @@
     <div>
 
         <h3 class="total_deg">Total Price: £{{$totalprice}}</h3>
+
+    </div>
+
+    <div>
+
+        <h3 style="font-size: 25px; padding-bottom: 15px">Proceed to checkout</h3>
+
+        <a href="{{url('cash_order')}}" class="btn btn-danger">Cash on Delivery</a>
+        <a href="" class="btn btn-danger">Pay With Card</a>
 
     </div>
 
